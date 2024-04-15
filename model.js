9aefe7ae36c2c62a42a67ebb6a41cc727afbf56b
Sushanth <126643807+Sushanth1805@users.noreply.github.com>
2024-04-15 12:15:11 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: String,
+    password: String,
+    googleId: String,
+    otp: String,
+  }, { timestamps: true });
+
+const foodSchema = new mongoose.Schema(
+  {
+    id: String,
+    name: String,
+    description: String,
+    price: Number,
+    image: String,
+    category: { type: String, enum: ["veg", "non-veg", "dessert"], default: "non-veg" },
+  }, { timestamps: true });
+
+const orderSchema = new mongoose.Schema(
+  {
+    foodId: String,
+    userId: String,
+    orderId: String,
+    status: String,
+    otpConfirmed: Boolean,
+    userAddressId: String,
+    paymentMode: { type: String, enum: ["cash", "card", "upi"], default: "cash" },
+    feedback: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    image: {
+      type: String,
+    },
+    textData: {
+      type: String,
+    },
+  }, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+const Food = mongoose.model("Food", foodSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+export { User, Food, Order };
